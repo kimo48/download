d1550a7c01ddc89fe312e1017ddf75835f719b5d
KIMO48 <103265597+kimo48@users.noreply.github.com>
2022-08-11 08:13:43 +0100

    Update download_tiktok.py

--- download_tiktok.py
+++ download_tiktok.py
@@ -6,9 +6,10 @@ url=str(sys.argv[1])
 
 d=ttdownloader(url)
 
-iid = findall(r'[0-9]{19}', url)[0]
+iid = findall(r'([a-zA-Z]\w+)', url)[4]
+
 print(iid)
 
-d[2].download(iid+'.mp3')
+d[2].download(str(iid)+'.mp3')
 
 #print (str(sys.argv[1]))
