e8e00b860d50a0c09373a1c0954fe754c8ecb0bf
KIMO48 <103265597+kimo48@users.noreply.github.com>
2022-08-11 06:37:33 +0100

    Update download_tiktok.py

--- download_tiktok.py
+++ download_tiktok.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
-
+from re import findall
 import sys
 from tiktok import ttdownloader
+url=str(sys.argv[1])
 
-d=ttdownloader(str(sys.argv[1]))
+d=ttdownloader(url)
 
-d[2].download('video.mp3')
+iid = findall(r'[0-9]{19}', url)[0]
+print(iid)
+
+d[2].download(iid+'.mp3')
 
 #print (str(sys.argv[1]))
