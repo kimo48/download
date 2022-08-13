6be1f9db849d82df8356687d8c63d2e6b7c3de2d
KIMO48 <103265597+kimo48@users.noreply.github.com>
2022-08-13 02:44:15 +0100

    Update download_youtube_title.py

--- download_youtube_title.py
+++ download_youtube_title.py
@@ -1,9 +1,12 @@
-import youtube_dl
+from bs4 import BeautifulSoup
+import requests
 import sys
 url=str(sys.argv[1])
 
-ydl_opts = {}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    info_dict = ydl.extract_info(url, download=False)
-    video_title = info_dict.get('title', None)
-    print(video_title.encode('unicode-escape').decode('utf-8'))
+def scrape_info(url):
+    r = requests.get(url)
+    s = BeautifulSoup(r.text, "html.parser")
+    title =s.find("meta", itemprop="name")['content']
+    print(title)
+  
+title= scrape_info(url)
