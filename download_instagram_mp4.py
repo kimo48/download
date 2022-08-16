1904db2ad3b493ca35ec12dc90909a65047dc02c
KIMO48 <103265597+kimo48@users.noreply.github.com>
2022-08-16 07:15:58 +0100

    Update download_instagram_mp4.py

--- download_instagram_mp4.py
+++ download_instagram_mp4.py
@@ -1,18 +1,110 @@
-from user_pwd import Username,Password,Path
-from instagrapi import Client
-import sys
-link=str(sys.argv[1])
+import requests,regex,sys,re
+import urllib.parse
+from datetime import datetime
+from tqdm import tqdm
+#from urllib import request
+#import requests
+#import requests as req
+#from urllib.request import Request, urlopen
 
-def download_fn(link):
-    try:
-        print("Downlaod start")
-        cl = Client()
-        cl.login(Username, Password)
-        info = cl.media_pk_from_url(link)
-        clip_url = cl.media_info(info).video_url
-        cl.clip_download_by_url(clip_url, folder = Path)
-        print("Downlaod done")
-    except:
-        pass
 
-download_fn(link)
+
+urlfb=str(sys.argv[1])
+#urlfb="https://www.instagram.com/reel/CguUAaZFptq/?igshid=YmMyMTA2M2Y="
+
+UA = {
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 11; Redmi Note 9 Pro Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.210 Mobile Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Referer': 'https://sssinstagram.com/https://www.instagram.com/reel/CguUAaZFptq',
+    'Accept-Language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Cookie': '_ga=GA1.2.2094519438.1640138973; _gid=GA1.2.1078295655.1640138975; _gat_gtag_UA_44090370_1=1'}
+
+
+data ={'url':urlfb}
+kimo0 = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+#with requests.Session() as s:
+response = requests.post(url='https://ssyoutube.com/api/convert',data = data, headers=kimo0)
+#print(response.url)
+html=urllib.parse.unquote_plus(response.text)
+#print(html) 
+#print("*******************************************************************************")
+raw_text = html
+start = 'uri='
+end = '&filename'
+start_index = re.search(r'\b' + start + r'\b', raw_text).start()
+end_index = re.search(r'\b' + end + r'\b', raw_text).end()
+trait_html = raw_text[start_index:end_index]
+#print(trait_html)
+size = len(trait_html)
+debut_char = trait_html[4:]
+size = len(debut_char)
+final_char = debut_char[:size - 9]
+#print(final_char)
+
+
+raw_text = html
+start = 'filename'
+end = '.mp4&'
+start_index = re.search(r'\b' + start + r'\b', raw_text).start()
+end_index = re.search(r'\b' + end + r'\b', raw_text).end()
+trait_html_title = raw_text[start_index:end_index]
+#print(trait_html_title)
+
+size = len(trait_html_title)
+debut_char_title = trait_html_title[9:]
+size = len(debut_char_title)
+final_char_title = debut_char_title[:size - 5]
+print(final_char_title)
+
+
+def download_video():
+
+    print("Download Start")
+    video_url =final_char                                             #regex.search(rf'{quality.lower()}_src:"(.+?)"', html).group(1)
+    file_size_request = requests.get(video_url, stream=True)
+    file_size = int(file_size_request.headers['Content-Length'])
+    block_size = 1024
+    locat_video="C:/Users/ssd/Desktop/"
+    filename = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
+    t = tqdm(total=file_size, unit='B', unit_scale=True, desc=filename, ascii=True)
+    with open(locat_video + final_char_title + '.mp4', 'wb') as f:
+        for data in file_size_request.iter_content(block_size):
+            t.update(len(data))
+            f.write(data)
+    t.close()
+    print("downloaded successfully")
+
+download_video()
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+url = 'https://sssinstagram.com/request'
+
+response = req.get(url)
+print(response)
+print("***********************************")
+headers = response.headers
+print(headers)
+print("***********************************")
+html = response.text
+print(headers)
+print("***********************************")
+
+'''
+
+'''
+with open('code.txt', 'w', encoding="utf-8") as f:
+    f.write(fbdown.decode('utf-8'))
+'''
+
