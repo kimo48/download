669c64691811c534580ea1106352317915722e07
KIMO48 <103265597+kimo48@users.noreply.github.com>
2022-08-16 07:16:19 +0100

    Delete user_pwd.py

--- user_pwd.py
+++
@@ -1,3 +0,0 @@
-Username = 'insta_test20222022'
-Password = 'insta48@insta48'
-Path = r'/data/data/com.termux/files/home/storage/shared/f_instagram/'#C:/Users/ssd/Desktop   #  /data/data/com.termux/files/home/storage/shared/f_instagram/
