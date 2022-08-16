91c0a67786544d89fb3b679be31a5ec29da1c4ed
KIMO48 <103265597+kimo48@users.noreply.github.com>
2022-08-17 00:44:07 +0100

    Update download_instagram_mp4.py

--- download_instagram_mp4.py
+++ download_instagram_mp4.py
@@ -8,44 +8,48 @@ from tqdm import tqdm
 #from urllib.request import Request, urlopen
 
 
+def scrap():
+    urlfb=str(sys.argv[1])
+    #urlfb="https://www.instagram.com/reel/CguUAaZFptq/?igshid=YmMyMTA2M2Y="
 
-urlfb=str(sys.argv[1])
-#urlfb="https://www.instagram.com/reel/CguUAaZFptq/?igshid=YmMyMTA2M2Y="
+    data ={'url':urlfb}
+    kimo0 = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-data ={'url':urlfb}
-kimo0 = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-
-response = requests.post(url='https://ssyoutube.com/api/convert',data = data, headers=kimo0)
-#print(response.url)
-html=urllib.parse.unquote_plus(response.text)
-#print(html) 
-#print("*******************************************************************************")
-raw_text = html
-start = 'uri='
-end = '&filename'
-start_index = re.search(r'\b' + start + r'\b', raw_text).start()
-end_index = re.search(r'\b' + end + r'\b', raw_text).end()
-trait_html = raw_text[start_index:end_index]
-#print(trait_html)
-size = len(trait_html)
-debut_char = trait_html[4:]
-size = len(debut_char)
-final_char = debut_char[:size - 9]
-#print(final_char)
-#print("*******************************************************************************")
-raw_text = html
-start = 'filename'
-end = '.mp4&'
-start_index = regex.search(r'\b' + start + r'\b', raw_text).start()
-end_index = regex.search(r'\b' + end + r'\b', raw_text).end()
-trait_html_title = raw_text[start_index:end_index]
-#print(trait_html_title)
-
-size = len(trait_html_title)
-debut_char_title = trait_html_title[9:]
-size = len(debut_char_title)
-final_char_title = debut_char_title[:size - 5]
-print(final_char_title)
+    response = requests.post(url='https://ssyoutube.com/api/convert',data = data, headers=kimo0)
+    #print(response.url)
+    html=urllib.parse.unquote_plus(response.text)
+    #print(html) 
+    #print("*******************************************************************************")
+    raw_text = html
+    start = 'uri='
+    end = '&filename'
+    
+try:
+    scrap()
+    start_index = re.search(r'\b' + start + r'\b', raw_text).start()
+    end_index = re.search(r'\b' + end + r'\b', raw_text).end()
+    trait_html = raw_text[start_index:end_index]
+    #print(trait_html)
+    size = len(trait_html)
+    debut_char = trait_html[4:]
+    size = len(debut_char)
+    final_char = debut_char[:size - 9]
+    #print(final_char)
+    #print("*******************************************************************************")
+    raw_text = html
+    start = 'filename'
+    end = '.mp4&'
+    start_index = regex.search(r'\b' + start + r'\b', raw_text).start()
+    end_index = regex.search(r'\b' + end + r'\b', raw_text).end()
+    trait_html_title = raw_text[start_index:end_index]
+    #print(trait_html_title)
+    size = len(trait_html_title)
+    debut_char_title = trait_html_title[9:]
+    size = len(debut_char_title)
+    final_char_title = debut_char_title[:size - 5]
+    print(final_char_title)
+except:
+    scrap()
 
 
 def download_video():
