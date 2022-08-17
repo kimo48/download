8effeb7d2ae6ea23b5a449fa6d1831f264c46775
KIMO48 <103265597+kimo48@users.noreply.github.com>
2022-08-17 01:20:51 +0100

    Update download_instagram_mp4.py

--- download_instagram_mp4.py
+++ download_instagram_mp4.py
@@ -28,7 +28,7 @@ def scrap():
     try:
         print("startttt")
         start = 'uri='
-        end = '&filename
+        end = '&filename'
         start_index = re.search(r'\b' + start + r'\b', raw_text).start()
         end_index = re.search(r'\b' + end + r'\b', raw_text).end()
         trait_html = raw_text[start_index:end_index]
