8863c14eec11e7b5fcc8317e8a9661450e19bf8c
KIMO48 <103265597+kimo48@users.noreply.github.com>
2022-08-17 01:12:23 +0100

    Update download_instagram_mp4.py

--- download_instagram_mp4.py
+++ download_instagram_mp4.py
@@ -7,15 +7,16 @@ from tqdm import tqdm
 #import requests as req
 #from urllib.request import Request, urlopen
 
+urlfb=str(sys.argv[1])
+#urlfb="https://www.instagram.com/reel/CguUAaZFptq/?igshid=YmMyMTA2M2Y="
+
 
 def scrap():
-    urlfb=str(sys.argv[1])
-    #urlfb="https://www.instagram.com/reel/CguUAaZFptq/?igshid=YmMyMTA2M2Y="
-
+    print("scrap")
     data ={'url':urlfb}
     kimo0 = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-
     response = requests.post(url='https://ssyoutube.com/api/convert',data = data, headers=kimo0)
+    print("scrap")
     #print(response.url)
     html=urllib.parse.unquote_plus(response.text)
     #print(html) 
@@ -24,38 +25,42 @@ def scrap():
     start = 'uri='
     end = '&filename'
     
-try:
-    scrap()
-    start_index = re.search(r'\b' + start + r'\b', raw_text).start()
-    end_index = re.search(r'\b' + end + r'\b', raw_text).end()
-    trait_html = raw_text[start_index:end_index]
-    #print(trait_html)
-    size = len(trait_html)
-    debut_char = trait_html[4:]
-    size = len(debut_char)
-    final_char = debut_char[:size - 9]
-    #print(final_char)
-    #print("*******************************************************************************")
-    raw_text = html
-    start = 'filename'
-    end = '.mp4&'
-    start_index = regex.search(r'\b' + start + r'\b', raw_text).start()
-    end_index = regex.search(r'\b' + end + r'\b', raw_text).end()
-    trait_html_title = raw_text[start_index:end_index]
-    #print(trait_html_title)
-    size = len(trait_html_title)
-    debut_char_title = trait_html_title[9:]
-    size = len(debut_char_title)
-    final_char_title = debut_char_title[:size - 5]
-    print(final_char_title)
-except:
-    scrap()
+    try:
+        print("startttt")
+        start = 'uri='
+        end = '&filename
+        start_index = re.search(r'\b' + start + r'\b', raw_text).start()
+        end_index = re.search(r'\b' + end + r'\b', raw_text).end()
+        trait_html = raw_text[start_index:end_index]
+        #print(trait_html)
+        size = len(trait_html)
+        debut_char = trait_html[4:]
+        size = len(debut_char)
+        final_char = debut_char[:size - 9]
+        print(final_char)
+        print("*******************************************************************************")
+        raw_text = html
+        start = 'filename'
+        end = '.mp4&'
+        start_index = regex.search(r'\b' + start + r'\b', raw_text).start()
+        end_index = regex.search(r'\b' + end + r'\b', raw_text).end()
+        trait_html_title = raw_text[start_index:end_index]
+        print(trait_html_title)
+        size = len(trait_html_title)
+        debut_char_title = trait_html_title[9:]
+        size = len(debut_char_title)
+        final_char_title = debut_char_title[:size - 5]
+        print(final_char_title)
+        download_video(final_char)
+    except:
+        print("erreur")
+        scrap()
 
 
-def download_video():
+def download_video(urll):
 
     print("Download Start")
-    video_url =final_char                                             #regex.search(rf'{quality.lower()}_src:"(.+?)"', html).group(1)
+    video_url =urll                                            #regex.search(rf'{quality.lower()}_src:"(.+?)"', html).group(1)
     file_size_request = requests.get(video_url, stream=True)
     file_size = int(file_size_request.headers['Content-Length'])
     block_size = 1024
@@ -68,7 +73,6 @@ def download_video():
     t.close()
     print("downloaded successfully")
 
-download_video()
 
 
 
@@ -100,4 +104,3 @@ print("***********************************")
 with open('code.txt', 'w', encoding="utf-8") as f:
     f.write(fbdown.decode('utf-8'))
 '''
-
