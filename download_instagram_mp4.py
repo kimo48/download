f4fc4901e159f54f14745b826b5eacff2a74430c
KIMO48 <103265597+kimo48@users.noreply.github.com>
2022-12-31 23:21:21 +0100

    Update download_instagram_mp4.py

--- download_instagram_mp4.py
+++ download_instagram_mp4.py
@@ -1,13 +1,14 @@
 import requests,sys,time,re
 import urllib.parse
-#from datetime import datetime
+from datetime import datetime
 from tqdm import tqdm
 #from urllib import request
 #import requests
 #import requests as req
 #from urllib.request import Request, urlopen
 
-urlfb=str(sys.argv[1])
+#urlfb=str(sys.argv[1])
+urlfb="https://www.instagram.com/reel/Ck5eiPugj0M/"
 #urlfb="https://www.instagram.com/reel/CguUAaZFptq/?igshid=YmMyMTA2M2Y="
 
 
@@ -21,56 +22,58 @@ def scrap():
     html=urllib.parse.unquote_plus(response.text)
     #print(html)
 
-    print("*******************************************************************************")
+    #print("*******************************************************************************")
+    
     raw_text = html
     start = 'uri='
     end = '&filename'
     
-    try:
-        print("start titre")
-        start_index = re.search(r'\b' + start + r'\b', raw_text).start()
-        #print(start_index)
-        end_index = re.search(r'\b' + end + r'\b', raw_text).end()
-        trait_html = raw_text[start_index:end_index]
-        #print(trait_html)
-        size = len(trait_html)
-        debut_char = trait_html[4:]
-        size = len(debut_char)
-        final_char = debut_char[:size - 9]
-        #print(final_char)
-        time.sleep(2)
-        #print("*******************************************************************************")
-        raw_text = html
-        start = 'filename='
-        end = '؟.mp4'
-        start_index = re.search(r'\b' + start + r'\b', raw_text).start()
-        #print(start_index)
-        end_index = re.search(r'\b' + end + r'\b', raw_text).end()
-        trait_html_title = raw_text[start_index:end_index]
-        #print(trait_html_title)
-        size = len(trait_html_title)
-        debut_char_title = trait_html_title[9:]
-        size = len(debut_char_title)
-        final_char_title = debut_char_title[:size - 5]
-        print(final_char_title)
-        return 0
-        time.sleep(2)
-        download_video(final_char)
-    except:
-        print("erreur")
-        time.sleep(2)
-        scrap()
+    #try:
+    print("start titre")
+    start_index = re.search(r'\b' + start + r'\b', raw_text).start()
+    #print(start_index)
+    end_index = re.search(r'\b' + end + r'\b', raw_text).end()
+    trait_html = raw_text[start_index:end_index]
+    #print(trait_html)
+    size = len(trait_html)
+    debut_char = trait_html[4:]
+    size = len(debut_char)
+    final_char = debut_char[:size - 9]
+    #print(final_char)
+    
+    time.sleep(2)
+    #print("*******************************************************************************")
+    raw_text = html
+    start = 'filename='
+    end = '.mp4&'
+    start_index = re.search(r'\b' + start + r'\b', raw_text).start()
+    #print(start_index)
+    end_index = re.search(r'\b' + end + r'\b', raw_text).end()
+    trait_html_title = raw_text[start_index:end_index]
+    #print(trait_html_title)
+    
+    size = len(trait_html_title)
+    debut_char_title = trait_html_title[9:]
+    size = len(debut_char_title)
+    final_char_title = debut_char_title[:size - 5]
+    print(final_char_title)
+    time.sleep(2)
+    download_video(final_char,"final_char_title")
+    #except:
+        #print("erreur")
+        #time.sleep(2)
+        #scrap()
 
-
-def download_video(urll):
+def download_video(urll,final_char_title):
 
     print("Download Start")
     video_url =urll                                            #regex.search(rf'{quality.lower()}_src:"(.+?)"', html).group(1)
     file_size_request = requests.get(video_url, stream=True)
     file_size = int(file_size_request.headers['Content-Length'])
     block_size = 1024
-    locat_video="/data/data/com.termux/files/home/storage/shared/f_instagram/"
-    t = tqdm(total=file_size, unit='B', unit_scale=True, desc=final_char_title, ascii=True)
+    filename = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
+    locat_video="/data/data/com.termux/files/home/storage/shared/f_instagram/"#"C:\\Users\\ssd\Desktop\\download-main"#"/data/data/com.termux/files/home/storage/shared/f_instagram/"
+    t = tqdm(total=file_size, unit='B', unit_scale=True, desc=filename, ascii=True)
     with open(locat_video + final_char_title + '.mp4', 'wb') as f:
         for data in file_size_request.iter_content(block_size):
             t.update(len(data))
@@ -78,8 +81,6 @@ def download_video(urll):
     t.close()
     print("downloaded successfully")
 
-    
-    
 scrap()
 
 
